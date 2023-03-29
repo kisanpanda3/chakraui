9d11cc038ad710624537e35f9e1f12f79b79bc6b
Kisan Kumar Panda <101393393+kisanpanda3@users.noreply.github.com>
2023-03-29 10:27:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import { RoutesPage } from "./Components/RoutesPage";
+export default function App() {
+  return (
+    <div className="App">
+      <RoutesPage />
+    </div>
+  );
+}
